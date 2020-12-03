87bea98f876943f9a8a57147898cdaa3b75c1eca
Yana <33156136+YanaSezko@users.noreply.github.com>
2020-12-03 19:17:34 +0200

    edit-12

--- Integration/addItemForm.test.js
+++ Integration/addItemForm.test.js
@@ -1,11 +1,11 @@
 describe('addItemForm', () => {
     it('base example, visually looks correct', async () => {
         // APIs from jest-puppeteer
-        await page.goto('http://localhost:6006/iframe.html?id=additemform-component--add-item-form-base-example&viewMode=story');
+        await page.goto('http://localhost:9009/iframe.html?id=additemform-component--add-item-form-base-example&viewMode=story');
         const image = await page.screenshot();
  
         // API from jest-image-snapshot
         expect(image).toMatchImageSnapshot();
-    });
+    },60000);
  });
  
